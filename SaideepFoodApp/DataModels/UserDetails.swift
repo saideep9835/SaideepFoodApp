93a1ba0c6b74d1f2c987ebad1a2094679ba4aac1
Saideep9835 <tsaideep98@gmail.com>
2024-09-18 15:49:38 -0500

    Written a protocol in SignUpViewModel and created a structure user details.

---
+++ SaideepFoodApp/DataModels/UserDetails.swift
@@ -0,0 +1,13 @@
+//
+//  UserDetails.swift
+//  SaideepFoodApp
+//
+//  Created by Saideep Reddy Talusani on 9/18/24.
+//
+
+import Foundation
+
+struct UserDetails{
+    let email: String
+    let password: String
+}
---
+++ SaideepFoodApp/SignIn/SignInViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewModel.swift
+//  SaideepFoodApp
+//
+//  Created by Saideep Reddy Talusani on 9/18/24.
+//
+
+import Foundation
+
+protocol SignInViewModel{
+    func signIn(email:String, password: String)
+}
